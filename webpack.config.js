d994476cd59ee39d9d3affc2a497755979c49074
Kevin <kevinvar2805@gmail.com>
2025-03-17 12:43:12 -0500

    Setup for TS

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   devtool: "inline-source-map",
   entry: path.join(__dirname,'client/src/index.jsx'),
   output: {
